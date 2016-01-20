0d390bd4c8b911654936facfd921a7a645ff045a
kairos-55 <jisus-55@hotmail.com>
2016-01-20 11:51:53 -0500

    Primera Versión del Proyecto

---
+++ ProyectoMIO/src/DataAccessObject/Fachada.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataAccessObject;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+/**
+ *
+ * @author jesusaro
+ */
+public class Fachada {
+    
+    private final String url;
+    private final String usuario;
+    private final String contraseña; 
+    private Connection conexion;
+    
+    public Fachada() {
+        
+        //Conexión a la base de datos JESUSARO de la Universidad 
+        url = "jdbc:postgresql://pgsql:5432/jesusaro";
+        usuario = "jesusaro";
+        contraseña = "jesusaro";
+        
+    }
+    
+    
+    
+    
+}
