c45b074ffc582318cdd8a538cde87cedc6aac9b4
kairos-55 <jisus-55@hotmail.com>
2016-01-25 11:25:15 -0500

    Métodos para guardar y modificar BUS

--- ProyectoMIO/src/DataAccessObject/DaoBus.java
+++ ProyectoMIO/src/DataAccessObject/DaoBus.java
@@ -5,12 +5,51 @@
  */
 package DataAccessObject;
 
+import Modelos.Excepciones;
+import Tablas.Bus;
+
 /**
  *
  * @author jesusaro
  */
 public class DaoBus extends DataAccessObject {
     
+    private String sql;
     
+    public DaoBus() {
+        
+        super();
+        
+    }
+    
+    public void guardarBus(Bus bus) throws Excepciones {
+        
+        sql = "INSERT INTO bus VALUES ('"
+                + bus.getPlaca() + "', '"
+                + bus.getModelo() + "', '"
+                + bus.getMarca() + "', '"
+                + bus.getCapacidad() + "', '"
+                + bus.getTipo() + "', '"
+                + bus.getParadaRuta() + "', '"
+                + bus.getNombreRuta() + "');";
+        
+        executeUpdate(sql);
+        
+    }
+    
+    public void modificarBus(Bus bus) throws Excepciones {
+        
+        sql = "UPDATE bus SET "
+                + "modelo = '" + bus.getModelo() + "', "
+                + "marca = '" + bus.getMarca() + "', "
+                + "capacidad = '" + bus.getCapacidad() + "', "
+                + "tipo = '" + bus.getTipo() + "', "
+                + "parada_ruta = '" + bus.getParadaRuta() + "', '"
+                + "nombre_ruta = '" + bus.getNombreRuta() + "' "
+                + "WHERE placa_bus = '" + bus.getPlaca() + "';";
+        
+        executeUpdate(sql);
+        
+    }
     
 }
