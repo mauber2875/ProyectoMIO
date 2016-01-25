5644d36cb53488406f72848df1fff1fd6069f489
kairos-55 <jisus-55@hotmail.com>
2016-01-25 11:17:32 -0500

    clase DataAccessObject terminada

--- ProyectoMIO/src/DataAccessObject/DataAccessObject.java
+++ ProyectoMIO/src/DataAccessObject/DataAccessObject.java
@@ -8,6 +8,7 @@ package DataAccessObject;
 import Modelos.Excepciones;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
@@ -19,8 +20,8 @@ public class DataAccessObject {
     
     protected final Fachada fachada;
     protected Connection conexion;
-    protected Statement consulta;
-    protected ResultSet resultado;
+    protected Statement declaracion;
+    protected ResultSet resultados;
     
     public DataAccessObject() {
         
@@ -28,10 +29,51 @@ public class DataAccessObject {
         
     }
     
-    protected void executeUpdate() throws Excepciones {
+    protected void executeUpdate(String sql) throws Excepciones {
         
-        conexion = fachada.;
+        try {
+            
+            conexion = fachada.conectar();
+            declaracion = conexion.createStatement();
+            int filasActualizadas = declaracion.executeUpdate(sql);
+            fachada.desconectar(conexion);
         
+        } catch (SQLException ex) {
+            throw new Excepciones(ex.getMessage());
+        }
+        
+    }
+    
+    protected ArrayList<Object[]> executeQuery(String sql, int columnas) throws Excepciones {
+        
+        try {
+            
+            ArrayList<Object[]> resultado = new ArrayList();
+            conexion = fachada.conectar();
+            declaracion = conexion.createStatement();
+            resultados = declaracion.executeQuery(sql);
+            
+            while(resultados.next()) {
+                
+                Object[] temporal = new Object[columnas];
+                
+                for(int i=0; i<columnas; i++) {
+                    
+                    temporal[i] = (String) resultados.getString(i+1);
+                    
+                }
+                
+                resultado.add(temporal);
+                
+            }
+            
+            fachada.desconectar(conexion);
+            
+            return resultado;
+            
+        } catch (SQLException ex) {
+            throw new Excepciones(ex.getMessage());   
+        }
         
     }
     
