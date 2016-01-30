26edf13e98e3d1de52eeda27fe84c357a851a6b9
mauber2875 <mauber2875@gmail.com>
2016-01-29 20:22:29 -0500

    Base de Datos Mauricio

--- Bases de Datos/ProyectoMIO.sql
+++ Bases de Datos/ProyectoMIO.sql
@@ -9,18 +9,18 @@ CREATE TABLE empleado
 	fecha_nacimiento DATE,
 	estado_civil VARCHAR(13),
 	cargo VARCHAR(12) NOT NULL,
-	eliminado CHAR NOT NULL,
+	estado_empleado  BOOLEAN NOT NULL,
 
 	CONSTRAINT empleado_pk PRIMARY KEY (cedula_empleado)
 );
 
 --Insertamos datos en la tabla EMPLEADO:
 
-INSERT INTO empleado VALUES (1000000, 690000, 'MAURICIO DOMINGUEZ', '1111111', '31-12-1980', 'VIUDO(A)', 'CONDUCTOR(A)', '0');
-INSERT INTO empleado VALUES (1000001, 690000, 'JESÚS RAMÍREZ', '2222222', '01-01-1990', 'SOLTERO(A)', 'AUXILIAR', '0');
-INSERT INTO empleado VALUES (1000002, 1500000, 'MARTHA HOLGUIN', '3333333', '01-05-1985', 'CASADO(A)', 'DIRECTOR(A)', '0');
-INSERT INTO empleado VALUES (1000003, 690000, 'ALEJANDRO MEJÍA', '4444444', '20-10-1970', 'UNIÓN LIBRE', 'CONDUCTOR', '0');
-INSERT INTO empleado VALUES (1000004, 690000, 'PEPITO PÉREZ', '5555555', '29-02-1988', 'DIVORCIADO(A)', 'CONDUCTOR', '0');
+INSERT INTO empleado VALUES (1000000, 690000, 'MAURICIO DOMINGUEZ', '1111111', '31-12-1980', 'VIUDO(A)', 'CONDUCTOR(A)', TRUE);
+INSERT INTO empleado VALUES (1000001, 690000, 'JESÚS RAMÍREZ', '2222222', '01-01-1990', 'SOLTERO(A)', 'AUXILIAR', TRUE );
+INSERT INTO empleado VALUES (1000002, 1500000, 'MARTHA HOLGUIN', '3333333', '01-05-1985', 'CASADO(A)', 'DIRECTOR(A)', TRUE );
+INSERT INTO empleado VALUES (1000003, 690000, 'ALEJANDRO MEJÍA', '4444444', '20-10-1970', 'UNIÓN LIBRE', 'CONDUCTOR', TRUE );
+INSERT INTO empleado VALUES (1000004, 690000, 'PEPITO PÉREZ', '5555555', '29-02-1988', 'DIVORCIADO(A)', 'CONDUCTOR', TRUE);
 
 ----------------------------------------------------------------------
 
@@ -28,21 +28,21 @@ DROP TABLE IF EXISTS ruta CASCADE;
 
 CREATE TABLE ruta
 (
-	parada_ruta INTEGER NOT NULL,
+	id_ruta INTEGER NOT NULL,
 	nombre_ruta VARCHAR(4) NOT NULL,
-	lugar VARCHAR(50) NOT NULL,
-	eliminada CHAR NOT NULL,
+	estado_ruta BOOLEAN NOT NULL,
+	
 
-	CONSTRAINT ruta_pk PRIMARY KEY (parada_ruta, nombre_ruta)
+	CONSTRAINT ruta_pk PRIMARY KEY (id_ruta)
 );
 
 --Insertamos datos en la tabla RUTA:
 
-INSERT INTO ruta VALUES (1, 'T31', 'CHIMINANGOS', '0');
-INSERT INTO ruta VALUES (2, 'T31', 'FLORA INDUSTRIAL', '0');
-INSERT INTO ruta VALUES (3, 'T31', 'FÁTIMA', '0');
-INSERT INTO ruta VALUES (1, 'A01', 'CALLE 30 # 15-28', '0');
-INSERT INTO ruta VALUES (2, 'A01', 'CARRERA 16 # 50-08', '0');
+INSERT INTO ruta VALUES (1, 'T31', 'CHIMINANGOS', TRUE);
+INSERT INTO ruta VALUES (2, 'T31', 'FLORA INDUSTRIAL', TRUE);
+INSERT INTO ruta VALUES (3, 'T31', 'FÁTIMA', TRUE);
+INSERT INTO ruta VALUES (1, 'A01', 'CALLE 30 # 15-28', TRUE);
+INSERT INTO ruta VALUES (2, 'A01', 'CARRERA 16 # 50-08', TRUE);
 
 ---------------------------------------------------------------------
 
@@ -54,18 +54,18 @@ CREATE TABLE distribuidor
 	nombre VARCHAR(50) NOT NULL,
 	direccion VARCHAR(50) NOT NULL,
 	telefono VARCHAR(10) NOT NULL,
-	eliminado CHAR NOT NULL,
+	estado_distribuidor BOOLEAN NOT NULL,
 
 	CONSTRAINT distribuidor_pk PRIMARY KEY (nit)
 );
 
 --Insertamos datos en la tabla DISTRIBUIDOR:
 
-INSERT INTO distribuidor VALUES ('1000000', 'GANE', 'CALLE 80 # 5-23', '1111111', '0');
-INSERT INTO distribuidor VALUES ('1000001', 'BALOTO', 'CALLE 81 # 6-24', '2222222', '0');
-INSERT INTO distribuidor VALUES ('1000002', 'ÉXITO', 'CALLE 82 # 7-25', '3333333', '0');
-INSERT INTO distribuidor VALUES ('1000003', 'OLIMPICA', 'CALLE 83 # 8-26', '4444444', '0');
-INSERT INTO distribuidor VALUES ('1000004', 'SUPER INTER', 'CALLE 84 # 9-27', '5555555', '0');
+INSERT INTO distribuidor VALUES ('1000000', 'GANE', 'CALLE 80 # 5-23', '1111111', TRUE);
+INSERT INTO distribuidor VALUES ('1000001', 'BALOTO', 'CALLE 81 # 6-24', '2222222', TRUE);
+INSERT INTO distribuidor VALUES ('1000002', 'ÉXITO', 'CALLE 82 # 7-25', '3333333', TRUE);
+INSERT INTO distribuidor VALUES ('1000003', 'OLIMPICA', 'CALLE 83 # 8-26', '4444444', TRUE);
+INSERT INTO distribuidor VALUES ('1000004', 'SUPER INTER', 'CALLE 84 # 9-27', '5555555', TRUE);
 
 ----------------------------------------------------------------------
 
@@ -73,29 +73,50 @@ DROP TABLE IF EXISTS estacion CASCADE;
 
 CREATE TABLE estacion
 (
-	nombre_estacion VARCHAR(20) NOT NULL,
+	id_estacion INTEGER NOT NULL,
+        nombre_estacion VARCHAR(20)
 	direccion VARCHAR(50) NOT NULL,
-	cedula_empleado VARCHAR(20) NOT NULL,
-	parada_ruta INTEGER NOT NULL,
-	nombre_ruta VARCHAR(50) NOT NULL,
-	eliminada CHAR NOT NULL,
+	cedula_empleado VARCHAR(20) NOT NULL,	
+	estado_estacion BOOLEAN NOT NULL,
 
-	CONSTRAINT estacion_pk PRIMARY KEY (nombre_estacion),
+	CONSTRAINT estacion_pk PRIMARY KEY (id_estacion),
 
-	CONSTRAINT estacion_fk1 FOREIGN KEY (cedula_empleado)
-	REFERENCES empleado (cedula_empleado),
-
-	CONSTRAINT estacion_fk2 FOREIGN KEY (parada_ruta, nombre_ruta)
-	REFERENCES ruta (parada_ruta, nombre_ruta)
+	
 );
 
 --Insertamos datos en la tabla ESTACION:
 
-INSERT INTO estacion VALUES ('UNIVALLE', 'CARRERA 100 # 16-04', '1000000', '01', 'T31', '0');
-INSERT INTO estacion VALUES ('CHIMINANGOS', 'CALLE 99 # 17-05', '1000001', '02', 'T31', '0');
-INSERT INTO estacion VALUES ('UNIDAD DEPORTIVA', 'CARRERA 101 # 15-03', '1000002', '03', 'T31', '0');
-INSERT INTO estacion VALUES ('BUITRERA', 'CALLE 98 # 18-05', '1000003', '01', 'A01', '0');
-INSERT INTO estacion VALUES ('CAPRI', 'CARRERA 102 # 14-02', '1000004', '02', 'A01', '0');
+INSERT INTO estacion VALUES (1,'UNIVALLE', 'CARRERA 100 # 16-04', '1000000', TRUE);
+INSERT INTO estacion VALUES (2,'CHIMINANGOS', 'CALLE 99 # 17-05', '1000001', TRUE);
+INSERT INTO estacion VALUES (3,'UNIDAD DEPORTIVA', 'CARRERA 101 # 15-03', '1000002', TRUE);
+INSERT INTO estacion VALUES (4,'BUITRERA', 'CALLE 98 # 18-05', '1000003', TRUE);
+INSERT INTO estacion VALUES (5,'CAPRI', 'CARRERA 102 # 14-02', '1000004', TRUE);
+
+----------------------------------------------------------------------
+
+DROP TABLE IF EXISTS parada CASCADE;
+
+CREATE TABLE parada
+(
+	id_parada INTEGER NOT NULL,
+        nombre_parada VARCHAR(20)
+	direccion VARCHAR(50) NOT NULL,		
+	estado_parada BOOLEAN NOT NULL,
+
+	CONSTRAINT estacion_pk PRIMARY KEY (id_parada),
+
+	
+);
+
+--Insertamos datos en la tabla ESTACION:
+
+INSERT INTO estacion VALUES (6,'MELENDEZ', 'CARRERA 1 # 6-04', TRUE);
+INSERT INTO estacion VALUES (7,'CALDAS', 'CALLE 9 # 7-05', TRUE);
+INSERT INTO estacion VALUES (8,'SILOE', 'CARRERA 10 # 15-03', TRUE);
+INSERT INTO estacion VALUES (9,'BELALCAZAR', 'CALLE 8 # 8-05', TRUE);
+INSERT INTO estacion VALUES (10,'CALIPSO', 'CARRERA 12 # 
+4-02', TRUE);
+
 
 ----------------------------------------------------------------------
 
@@ -103,19 +124,19 @@ DROP TABLE IF EXISTS tarjeta CASCADE;
 
 CREATE TABLE tarjeta
 (
-	id_tarjeta VARCHAR(20) NOT NULL,
+	id_tarjeta INTEGER NOT NULL,
 	saldo DOUBLE PRECISION NOT NULL,
-	estado VARCHAR(8) NOT NULL,
-	punto_venta VARCHAR(20) NOT NULL,
-	eliminada CHAR NOT NULL,
+	estado_tarjeta VARCHAR(8) NOT NULL,
+	id_punto_venta INTEGER NOT NULL,
+	fecha_venta DATE NOT NULL
 
 	CONSTRAINT tarjeta_pk PRIMARY KEY (id_tarjeta),
 
-	CONSTRAINT tarjeta_fk1 FOREIGN KEY (punto_venta) 
-	REFERENCES estacion (nombre_estacion),
+	CONSTRAINT tarjeta_fk1 FOREIGN KEY (id_punto_venta) 
+	REFERENCES estacion (id_estacion),
 
-	CONSTRAINT tarjeta_fk2 FOREIGN KEY (punto_venta) 
-	REFERENCES distribuidor (nit)
+	CONSTRAINT tarjeta_fk2 FOREIGN KEY (id_punto_venta) 
+	REFERENCES sucursal (nit)
 );
 
 ---------------------------------------------------------------------
@@ -125,11 +146,11 @@ DROP TABLE IF EXISTS usuario CASCADE;
 CREATE TABLE usuario
 (
 	cedula_usuario VARCHAR(20) NOT NULL,
-	id_tarjeta VARCHAR(20) NOT NULL,
+	id_tarjeta INTEGER NOT NULL,
 	nombre VARCHAR(50) NOT NULL,
 	telefono VARCHAR(10),
 	direccion VARCHAR(50),
-	eliminado CHAR NOT NULL,
+	estado_usuario BOOLEAN NOT NULL,
 	
 	CONSTRAINT usuario_pk PRIMARY KEY (cedula_usuario),
 
@@ -148,14 +169,13 @@ CREATE TABLE bus
 	marca VARCHAR(20),
 	capacidad INTEGER,
 	tipo VARCHAR(13) NOT NULL,
-	parada_ruta INTEGER NOT NULL,
-	nombre_ruta VARCHAR(4) NOT NULL,
-	eliminado CHAR NOT NULL,
+	id_ruta INTEGER NOT NULL,	
+	estado_bus BOOLEAN NOT NULL,
 
 	CONSTRAINT bus_pk PRIMARY KEY (placa_bus),
 
-	CONSTRAINT bus_fk FOREIGN KEY (parada_ruta, nombre_ruta)
-	REFERENCES ruta (parada_ruta, nombre_ruta) 
+	CONSTRAINT bus_fk FOREIGN KEY (id_ruta)
+	REFERENCES ruta (id_ruta) 
 );
 
 ---------------------------------------------------------------------
